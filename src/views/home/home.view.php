25e64f539cfdd27c7b910b22b29029c64d1680e5
Joseph Martire <joemart06@gmail.com>
2025-10-21 13:12:24 -0400

    Add header and footer

--- src/views/home/home.view.php
+++ src/views/home/home.view.php
@@ -1,32 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task Management System with User Roles</title>
-    <link rel="stylesheet" type="text/css" href="./views/home/home.css">
-</head>
-<body>
-    <nav class="navbar">
-        <div>Home</div>
-        <ul class="navbar_account">
-            
-            <!-- If not logged in -->
-             <?php if (!false): ?>
-            <li>Log in</li>
-            <li>Register</li>
-            <?php endif; ?>
-
-            <!-- If logged in -->
-            <?php if (!true): ?>
-            <li>Log out</li>
-            <li>Account</li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+<?php require_once('./includes/header.php'); ?>
+    
     <div class="container">
         <h1>Task Management System with User Roles</h1>
     </div>
-    
-</body>
-</html>
+
+
+<?php require_once('./includes/footer.php'); ?>
