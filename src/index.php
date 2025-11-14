cb4b911feb840caa0f9b9d9631f19692f34e9cb0
Joseph Martire <joemart06@gmail.com>
2025-11-14 14:25:39 -0400

    Remove UserAuthenticationController

--- src/index.php
+++ src/index.php
@@ -3,7 +3,6 @@
 require __DIR__ . "/config/constants.php";
 require BASE_PATH . "/includes/functions.php";
 require "./config/database.php";
-require_once BASE_PATH . "/controllers/UserAuthenticationController.php";
 
 require "./includes/router.php";
 
